69f7c4e86c1af50b5a8a4c9f2396f1f0360be37f
Sawaez Amin <sawaezamin@gmail.com>
2021-07-14 15:16:56 -0600

    Finish iteration 3

--- lib/user.rb
+++ lib/user.rb
@@ -6,6 +6,16 @@ class User
   end
 
   def learn(joke)
-    @jokes << joke 
+    @jokes << joke
+  end
+
+  def tell(user, joke)
+    user.learn(joke)
+  end
+
+  def joke_by_id(id)
+    @jokes.detect do |joke|
+      joke.id == id
+    end
   end
 end
